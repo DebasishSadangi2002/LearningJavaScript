0348a6e4c7a5d880c3e8241b14fab97f20611a0f
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-11 11:22:20 +0000

    opearations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,65 @@
+// Addition
+let a = 10;
+let b = 5;
+let sum = a + b; // sum is 15
+
+// Subtraction
+let difference = a - b; // difference is 5
+
+// Multiplication
+let product = a * b; // product is 50
+
+// Division
+let quotient = a / b; // quotient is 2
+
+// Modulus (remainder)
+let remainder = a % b; // remainder is 0
+
+// Exponentiation (Power)
+let power = a ** 2; // power is 100
+
+// Concatenation
+let str1 = "Hello";
+let str2 = "World";
+let greeting = str1 + " " + str2; // greeting is "Hello World"
+
+// String Length
+let length = greeting.length; // length is 11
+
+// Substring
+let subStr = greeting.substring(0, 5); // subStr is "Hello"
+
+// Extracting a part using slice
+let slicedStr = greeting.slice(6); // slicedStr is "World"
+
+// Uppercase
+let upperCaseStr = greeting.toUpperCase(); // upperCaseStr is "HELLO WORLD"
+
+// Lowercase
+let lowerCaseStr = greeting.toLowerCase(); // lowerCaseStr is "hello world"
+
+// Replace
+let replacedStr = greeting.replace("World", "JavaScript"); // replacedStr is "Hello JavaScript"
+
+// Unary Plus (+)
+let numStr = "42";
+let num = +numStr; // num is 42, converts string to number
+
+// Unary Negation (-)
+let positive = 5;
+let negative = -positive; // negative is -5
+
+// Increment (++)
+let count = 0;
+count++; // count is 1, increments value by 1
+
+// Decrement (--)
+let score = 10;
+score--; // score is 9, decrements value by 1
+
+// Logical NOT (!)
+let isTrue = true;
+let isFalse = !isTrue; // isFalse is false
+
+// Bitwise NOT (~)
+let bitwiseNot = ~5; // bitwiseNot is -6 (inverts bits)
