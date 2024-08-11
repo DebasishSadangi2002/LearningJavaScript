b6c4f6efa41858ba2064a2d6bbd27bfa138d13f2
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-11 11:39:32 +0000

    datatupe summery

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,11 @@
+// Primitive datatypes are call by value
+// 1.String
+// 2.Number
+// 3.boolean
+// 4.null
+// 5.undefined
+// 6.symbol
+// 7.BigInt
+
+// Reference (Non primitive)
+// Array, Object, Functions
