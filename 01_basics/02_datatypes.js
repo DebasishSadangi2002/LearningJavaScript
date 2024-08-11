2c6edb849deba53fc176647f29888ac42f2d5a8a
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-11 10:47:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let namE = "Debasish" //String
+let age = 22 //integer
+let isLoggedIn = true //boolean either TRUE or FALSE
+// null => standalone value
+// undefined => not assigned value yet
+// symbol => uniqueness
+// object 
+
+console.table([(typeof namE),(typeof age),(typeof isLoggedIn)])
+
+console.log(typeof undefined);//datatype undefined
+console.log(typeof null);//datatype object
