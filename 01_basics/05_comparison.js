1bc0fdd336a69692f6ea71c8a1c596993c1c2b3d
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-11 11:31:36 +0000

    comparision

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,10 @@
+let a = 5
+let b = '5'
+console.log(a==b) // == only compares values after type conversion
+console.log(a===b) // === compares both datatype and value
+console.log(a!=b) // != not equal to operator
+
+let x = 10;
+let y = 5;
+console.log(x > y); // true, because 10 is greater than 5
+console.log(x >=y); 
