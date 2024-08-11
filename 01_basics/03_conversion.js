40994e79777f4f61de4ae2f830a306d77db8206b
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-11 11:12:59 +0000

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let score ;
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let booleanValue = Boolean(score)
+// console.log(typeof booleanValue)
+// console.log(booleanValue)
+
+// true => 1; false => 0;
+// "Debasish" => true
+// "" => false
+// 33 => true
+// null => false
+// undefined => false
+
+let stringValue = String(score)
+console.log(typeof stringValue);
+console.log(stringValue);
+
+// 33 => "33"
+// true => "true" ; false => "false"
+// null => "null"
+// undefined => "undefined"
