a4e24663881e1d24e7330a7a2bcba6d713fa8e74
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-16 03:12:26 +0000

    object destructuring

---
+++ 02_basics/05_object_destructuring.js
@@ -0,0 +1,10 @@
+// object dectructuring
+const course = {
+    name : 'JavaScript',
+    instructure: 'Debasish',
+    fee: 999,
+}
+
+const {instructure : teacher} = course
+//console.log(teacher)
+
