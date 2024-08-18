65ddfa3b2fa35d5ecdc442ce3cefee9b8848511f
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-18 13:35:03 +0000

    control flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,30 @@
+const num = 5;
+ 
+if (num > 0) {
+    console.log("The number is positive.");
+};
+
+let num1 = -10;
+ 
+if (num1 > 0)
+    console.log("The number is positive.");
+else
+    console.log("The number is negative");
+
+
+
+    let num = 5;
+ 
+    switch (num) {
+        case 0:
+            console.log("Number is zero.");
+            break;
+        case 1:
+            console.log("Nuber is one.");
+            break;
+        case 2:
+            console.log("Number is two.");
+            break;
+        default:
+            console.log("Number is greater than 2.");
+    };
