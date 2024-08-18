b2db273f4a2b78491ba959ea56c33194f5b40b5b
Debasish Sadangi <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-08-18 04:40:06 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,9 @@
+if (true){
+let a = 30
+const b = 20
+var c = 10
+}
+
+console.log(a) // undefined as it is not delared in global scope
+console.log(b) // undefined as it is not delared in global scope
+console.log(c) // showing value 30
